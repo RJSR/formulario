0cbe4a22fbae390bcc90a3798769796548edb4d2
RJSR <r.salazar122@gmail.com>
2023-09-16 18:32:44 -0400

    Intento 2

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -17,11 +17,11 @@ const showingNavigationDropdown = ref(false);
 </script>
 
 <template>
-    <div>
+    <div v-for="color, i in colors" :key="color.id">
         <div class="min-h-screen bg-gray-100" >
-            <nav class="bg-white border-b border-gray-100" :style="{ backgroundColor: color.thirdcolor, color: color.fontcolor }"  >
+            <nav class="bg-white border-b border-gray-100"   >
                 <!-- Primary Navigation Menu -->
-                <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8"  >
+                <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" :style="{ backgroundColor: color.thirdcolor, color: color.fontcolor }" >
                     <div class="flex justify-between h-16">
                         <div class="flex">
                             <!-- Logo -->
