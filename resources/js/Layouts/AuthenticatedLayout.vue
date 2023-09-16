070c773864fba74ca37ec62ba387a92b56732cd9
RJSR <r.salazar122@gmail.com>
2023-09-15 23:47:12 -0400

    Dashboard color 1st try

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -7,7 +7,11 @@ import NavLink from '@/Components/NavLink.vue';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
 import { Link } from '@inertiajs/vue3';
 
+const props = defineProps({
+    color:{type:Object,default:() => ({})},
+    colors:{type:Object}
 
+});    
 
 const showingNavigationDropdown = ref(false);
 </script>
@@ -15,7 +19,7 @@ const showingNavigationDropdown = ref(false);
 <template>
     <div>
         <div class="min-h-screen bg-gray-100" >
-            <nav class="bg-white border-b border-gray-100"  >
+            <nav class="bg-white border-b border-gray-100" :style="{ backgroundColor: color.thirdcolor, color: color.fontcolor }"  >
                 <!-- Primary Navigation Menu -->
                 <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8"  >
                     <div class="flex justify-between h-16">
